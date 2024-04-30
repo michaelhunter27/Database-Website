c6da146de4029310100fdc3582b219b712f98972
michaelhunter27 <michael.max.hunter27@gmail.com>
2024-04-30 16:28:32 -0700

    Add DDL.sql

---
+++ DDL.sql
@@ -0,0 +1,33 @@
+-- Ryan Giard and Michael Hunter
+-- CS 340 Group 91
+
+SET FOREIGN_KEY_CHECKS=0;
+SET AUTOCOMMIT = 0;
+
+-- Data Definition Queries
+-- Accounts
+-- CREATE OR REPLACE TABLE Accounts ...
+
+-- Characters
+
+-- Classes
+
+-- Guilds
+
+-- Hats
+
+
+-- Sample Data Insert Statements
+-- Accounts
+-- INSERT INTO TABLE Accounts VALUES ...
+
+-- Characters
+
+-- Classes
+
+-- Guilds
+
+-- Hats
+
+SET FOREIGN_KEY_CHECKS=1;
+COMMIT;
