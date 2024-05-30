e3e528f054442747951c44c11f2e20887e1aed60
Ryan Giard <giardr@oregonstate.edu>
2024-05-30 14:39:02 -0700

    guild fixes

--- App/routes/guilds.js
+++ App/routes/guilds.js
@@ -2,28 +2,67 @@ const express = require('express');
 const router = express.Router();
 const db = require('../database/db-connector');
 
-router.get('/add', function (req, res) {
-    res.render('addguild');
+router.get('/', (req, res) => {
+    const guildsQuery = `SELECT * FROM Guilds;`;
+
+    db.query(guildsQuery, (err, results) => {
+        if (err) { 
+            console.error('Error fetching guilds:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            const guilds = results;
+            res.render('guilds', { guilds });
+        }
+    });
 });
 
-router.post('/add', function (req, res) {
+router.post('/add', (req, res) => {
     const { name, creation_date } = req.body;
+    const addGuildQuery = `INSERT INTO Guilds (name, creation_date) VALUES ('${name}', '${creation_date}');`;
 
-    if (!name || !creation_date) {
-        return res.status(400).render('addguild', { error: 'Name and creation date are required.' });
-    }
-
-    const insertQuery = `
-        INSERT INTO Guilds (name, creation_date)
-        VALUES (?, ?)
-    `;
-    db.query(insertQuery, [name, creation_date], function (err, result) {
+    db.query(addGuildQuery, (err, results) => {
         if (err) {
             console.error('Error inserting guild:', err);
-            return res.status(500).send('Error adding guild.');
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.redirect('/guilds');
         }
-       
-        res.redirect('/guilds');
+    });
+});
+
+router.put('/update', (req, res) => {
+    const { guildID, name, creation_date } = req.body;
+    const updateGuildQuery = `UPDATE Guilds SET name = '${name}', creation_date = '${creation_date}' WHERE guildID = ${guildID};`;
+
+    db.query(updateGuildQuery, (err, result) => {
+        if (err) {
+            console.error('Error updating guild:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            const selectGuildQuery = `SELECT * FROM Guilds WHERE guildID = ${guildID};`;
+            db.query(selectGuildQuery, (err, result) => {
+                if (err) {
+                    console.error('Error fetching updated guild:', err);
+                    res.status(500).send('Internal Server Error');
+                } else {
+                    res.send(result);
+                }
+            });
+        }
+    });
+});
+
+router.delete('/delete', (req, res) => {
+    const { guildID } = req.body;
+    const deleteGuildQuery = `DELETE FROM Guilds WHERE guildID = ${guildID};`;
+
+    db.query(deleteGuildQuery, (err, results) => {
+        if (err) {
+            console.error('Error deleting guild:', err);
+            res.status(500).send('Internal Server Error');
+        } else {
+            res.sendStatus(204);
+        }
     });
 });
 
