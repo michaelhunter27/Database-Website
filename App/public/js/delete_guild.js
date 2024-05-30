2c7b33f2763e8d11b7cc3e029f9e33f630ffbcd4
Ryan Giard <giardr@oregonstate.edu>
2024-05-30 15:31:30 -0700

    test

---
+++ App/public/js/delete_guild.js
@@ -0,0 +1,30 @@
+
+function deleteGuild(guildID) {
+    const data = {
+        guildID: guildID
+    };
+
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("DELETE", "/guilds/delete", true);
+    xhttp.setRequestHeader("Content-type", "application/json");
+
+    xhttp.onreadystatechange = () => {
+        if (xhttp.readyState == 4 && xhttp.status == 204) {
+            deleteGuildRow(guildID);
+        } else if (xhttp.readyState == 4 && xhttp.status != 204) {
+            console.log("There was an error with the input.");
+        }
+    };
+
+    xhttp.send(JSON.stringify(data));
+}
+
+function deleteGuildRow(guildID) {
+    let table = document.querySelector("table");
+    for (let i = 1, row; row = table.rows[i]; i++) {
+        if (table.rows[i].getAttribute("guild-id") == guildID) {
+            table.deleteRow(i);
+            break;
+        }
+    }
+}
